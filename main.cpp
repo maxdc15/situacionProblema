24a5a39d915074724c1d48fb0dc868cb978082f5
Gheutx <a01798048@tec.mx>
2023-06-30 00:07:48 -0600

    Recuerdo

--- main.cpp
+++ main.cpp
@@ -3,9 +3,6 @@ Autor: Maximiliano De La Cruz Lima
 Main donde se puede probar la plataforma de streaming
 */
 
-#include "Video.h"
-#include "Pelicula.h"
-#include "Episodio.h"
 #include "Sistema.h"
 
 int main(int argc, char const *argv[])
